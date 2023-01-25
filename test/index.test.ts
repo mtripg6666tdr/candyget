892dd962c756702612311ec4316b1fce1a71cf16
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2023-01-25 10:04:07 +0900

    Fix: test

--- test/index.test.ts
+++ test/index.test.ts
@@ -494,8 +494,9 @@ describe("CandyGet Tests", function(){
       const scope = nock(nockUrl(), {
         reqheaders: {
           "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "accept-Language": "*",
           "accept-encoding": "gzip, deflate, br",
-          "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+          "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Candyget/0.0.0",
         }
       })
         .get("/get")
