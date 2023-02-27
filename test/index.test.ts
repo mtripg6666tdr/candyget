194748e2aac381218d932bf94b126455e1497f1f
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2023-02-27 14:33:17 +0900

    Fix: decompress multiple encodings (#92)
    * Fix: decompress multiple encodings
    
    * Chore: fix test
    
    * Chore: fix test

--- test/index.test.ts
+++ test/index.test.ts
@@ -487,6 +487,55 @@ describe("CandyGet Tests", function(){
         assert.equal(sha256Checksum(result.body).toString("hex").toUpperCase(), JQUERY_HASH);
       });
     });
+
+    describe("#gzip identity", function(){
+      it("correct body", async function(){
+        const scope = nock(nockUrl())
+          .get("/gzip")
+          .reply(200, () => fs.createReadStream(path.join(__dirname, "./jquery.min.js")).pipe(zlib.createGzip()), {
+            "Content-Encoding": "gzip, identity",
+          });
+        const result = await candyget(nockUrl("/gzip"), "string");
+        scope.done();
+        assert.equal(sha256Checksum(result.body).toString("hex").toUpperCase(), JQUERY_HASH);
+      });
+    });
+
+    describe("#deflate gzip", function(){
+      it("correct body", async function(){
+        const scope = nock(nockUrl())
+          .get("/deflate-gzip")
+          .reply(
+            200,
+            () => fs.createReadStream(path.join(__dirname, "./jquery.min.js"))
+              .pipe(zlib.createDeflate())
+              .pipe(zlib.createGzip()),
+            {
+              "Content-Encoding": "deflate, gzip",
+            }
+          );
+        const result = await candyget(nockUrl("/deflate-gzip"), "string");
+        scope.done();
+        assert.equal(sha256Checksum(result.body).toString("hex").toUpperCase(), JQUERY_HASH);
+      });
+    });
+
+    describe("#deflate gzip", function(){
+      it("correct body", async function(){
+        const scope = nock(nockUrl())
+          .get("/unknown")
+          .reply(
+            200,
+            () => Buffer.from(Array.from({length: 7}, (_, i) => 36 * i)),
+            {
+              "Content-Encoding": "super-compression, identity"
+            }
+          );
+        const result = await candyget(nockUrl("/unknown"), "buffer");
+        scope.done();
+        expect(result.body).to.deep.equal(Buffer.from(Array.from({length: 7}, (_, i) => 36 * i)));
+      });
+    });
   });
 
   describe("#Sending Default Headers", function(){
